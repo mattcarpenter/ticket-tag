1027bc00d31d326499536aa7cf73e2b00372e04c
Matt Carpenter <Matt Carpenter>
2017-07-04 19:38:10 -0700

    expose methods

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const ticketTag = require('./lib/ticket-tag');
 const fs = require('fs');
 
-const image = fs.readFileSync('./board9.jpg');
-const out = ticketTag.extractFromImage(image);
-fs.writeFileSync('./out.jpg', out[0].data, 'binary');
+module.exports = {
+  extractFromImage: ticketTag.extractFromImage,
+  create: ticketTag.create
+};
 
-//var img = ticketTag.create("DIDFE-1922");
-//console.log(img);
-//fs.writeFileSync('./tag.jpg', img, 'binary');
